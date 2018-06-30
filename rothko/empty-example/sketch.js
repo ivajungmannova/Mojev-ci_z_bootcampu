442f6388efb5fc5d846badd158649b5ad6f3255f
Unknown <jungmannova.iva@gmail.com>
2018-06-30 11:13:34 +0200

    Solární soustava

--- rothko/empty-example/sketch.js
+++ rothko/empty-example/sketch.js
@@ -1,50 +1,69 @@
 // do not remove, allowes autocomplete
 /// <reference path="./../p5.global-mode.d.ts" />
-var clicks = 10;
-var keys = 15;
-var time = 10;
-var timespeed = 1;
-var mycolor = 0;
+var zeme;
+var venuse;
+var merkur;
+var mars;
+var jupiter;
+var saturn;
+var uran;
+
+
+
 function setup() {
   createCanvas(400, 400);
-  colorMode(HSB);
  
+  zeme = createVector(0, 100);
+  
+  venuse = createVector(0, 80);
+  
+  merkur = createVector(0, 60);
+  mars = createVector(0, 120);
+  jupiter = createVector(0, 170);
+  saturn = createVector(0, 190);
+  uran = createVector(0, 200);
+  prstenec = createVector(0, 190);
+  
+  
 }
 
-function draw() {
-  time = time + timespeed;
-  var ellipseOffset = time % 400;
-  var mycolor = time % 400;
-  if (ellipseOffset>30) {
-    background(0);
-}else{
-  background(160, 23, 57, 59);
-  
-    }
-    if (keyIsPressed) {
-      timespeed = 10;
-    } else {
-      timespeed = 1;
-    }
-  background(50);
-  fill(50, 100, 80);
-  ellipse(ellipseOffset, 56, 58 + clicks, 60 + keys);
-  fill(mycolor, 100, 100);
-  triangle(260, 300, 330, 100, 70 + clicks, 120 + keys);
-  fill("white");
-  rect(80, 320, 90 + clicks, 80 + clicks, 20);
-  fill("orange");
+function draw() { 
+  background("#262626");
+  translate(200, 200);
+  fill("#F7FE2E");
+  ellipse(0, 0, 100);
 
-  arc(100, 120, 80 + keys, 80 + clicks, 0, PI + QUARTER_PI);
+  zeme.rotate(0.02);
+  venuse.rotate(-0.02);
+  merkur.rotate(0.03);
+  mars.rotate(-0.02);
+  jupiter.rotate(0.01);
+  saturn.rotate(-0.01);
+  uran.rotate(0.008);
+  prstenec.rotate(-0.01);
+  
+
+  fill("#01DFD7");
+  ellipse(zeme.x, zeme.y, 10);
+  fill("#61380B");
+  ellipse(venuse.x,venuse.y, 11);
+  fill("#FE9A2E");
+  ellipse(merkur.x, merkur.y, 8);
+fill("#B40404")
+  ellipse(mars.x, mars.y, 12);
+  fill("#FF8000")
+  ellipse(jupiter.x, jupiter.y, 23);
+  fill("#ff9900");
+  ellipse(saturn.x, saturn.y, 21);
+  fill("#336699");
+  ellipse(uran.x, uran.y, 14);
+  fill("#ff9900");
+  ellipse(prstenec.x, prstenec.y, 45, 3);
  
-  
-  
+
+
+ }
+ 
+ 
   
   // put drawing code here
-}
-function mouseReleased() {
-  clicks = clicks + 8;
-}
-function keyReleased() {
-  keys = keys + 8;
-}
